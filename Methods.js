9976a095967ed7a74390c0ba41e4a221be69519a
Dennis <dennis.heininger@mnd.thm.de>
2022-01-29 20:41:56 +0100

    Fehler beheben in js

--- Methods.js
+++ Methods.js
@@ -12,7 +12,7 @@ var y;
 var datum;
 var datumend;
 var zeit;
-var eintraege;
+
 
 input.addEventListener('change', start => {
     let files = input.files;
@@ -35,10 +35,16 @@ input.addEventListener('change', start => {
             }
         }
         console.log(counter);
+        //https://www.youtube.com/watch?v=tMeDkp1J2OM
+
+        const myarray = [];
+
         counter = 0
+
         for(i = 0; i < lines.length; i++){
 
             if(lines[i] == 'BEGIN:VEVENT'){
+                myarray[counter] = [];
 
                 while(lines[i] != 'END:VEVENT'){
 
@@ -51,57 +57,50 @@ input.addEventListener('change', start => {
                         datum = datum[0];
                         datum = datum.slice(0, 4) + "-" + datum.slice(4,6) + "-" +  datum.slice(6,8);
 
-                        console.log("DTSTART;TZID=Europe/Berlin");
-                        console.log(zeit);
-                        console.log(datum);
-
-                        eintraege [counter][0] = datum;
-                        eintraege [counter][1] = zeit;
+                        myarray[counter][0] = datum;
+                        myarray[counter][1] = zeit;
 
                     } else if (y[0] === 'DTEND;TZID=Europe/Berlin') {
-                        datumend = y[1].split("T")
+                        datumend = y[1].split("T");
                         zeit = datumend [1];
                         zeit = zeit.slice(0, 2) + ":" + zeit.slice(2,4) + ":" +  zeit.slice(4,6);
-                        console.log("DTend;TZID=Europe/Berlin: ")
-                        console.log(zeit);
 
-                        eintraege [counter][3] = zeit;
+                        myarray [counter][2] = zeit;
 
                     } else if (y[0] === 'CATEGORIES') {
-                        console.log("categories: ");
-                        console.log(y[1]);
-                        eintraege [counter][4] = y[1];
+
+                        myarray[counter][3] = y[1];
                     } else if (y[0] === 'DESCRIPTION') {
-                        console.log("description: ");
-                        console.log(y[1]);
-                        eintraege [counter][5] = y[1];
+
+                        myarray[counter][4] = y[1];
 
                     } else if (y[0] === 'LOCATION') {
-                        console.log("location: ");
-                        console.log(y[1]);
-                        eintraege [counter][6] = y[1];
+
+                        myarray[counter][5] = y[1];
 
                     } else if (y[0] === 'SUMMARY') {
-                        console.log("summary: ");
-                        console.log(y[1]);
-                        eintraege [counter][7] = y[1];
+
+                        myarray[counter][6] = y[1];
 
                     } else if (y[0] === 'COMMENT') {
-                        console.log("comment: ");
-                        console.log(y[1]);
-                        eintraege [counter][8] = y[1];
+
+                        myarray[counter][7] = y[1];
                     }
                     i++;
                 }
                 counter ++;
             }
         }
+
+        for(i = 0; i < counter;i++){
+            console.log("Neuer Eintrag!")
+            for(y = 0; y < 8; y++){
+                console.log(myarray[i][y]);
+            }
+
+
+        }
     }
-
-
-
-
-
 
 
     reader.onerror = (e) => alert(e.tag.error.name);
